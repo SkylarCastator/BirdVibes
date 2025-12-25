9dd63a40459258349424b9fa096eddd76a942c9d
SkylarCastator <aerialchemist@gmail.com>
2025-12-25 14:51:57 +1300

    add Node.js and frontend build to install process
    - install Node.js 18 from nodesource
    - build React frontend during installation
    - update newinstaller.sh repo URL placeholder

--- newinstaller.sh
+++ newinstaller.sh
@@ -44,7 +44,8 @@ if [[ ! -z $PACKAGES_MISSING ]] ; then
 fi
 
 branch=main
-git clone -b $branch --depth=1 https://github.com/Nachtzuster/BirdNET-Pi.git ${HOME}/BirdNET-Pi &&
+# Change this URL to your BirdVibes fork
+git clone -b $branch --depth=1 https://github.com/YOUR_USERNAME/BirdVibes.git ${HOME}/BirdNET-Pi &&
 
 $HOME/BirdNET-Pi/scripts/install_birdnet.sh
 if [ ${PIPESTATUS[0]} -eq 0 ];then
